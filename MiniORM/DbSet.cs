98d33b1a51683474e7b2d357d8f03bb8f029c51f
yana.radeva7 <yana.radeva7@gmail.com>
2022-07-20 19:04:19 +0300

    implement DbSet and DbContext

--- MiniORM/DbSet.cs
+++ MiniORM/DbSet.cs
@@ -1,5 +1,81 @@
-﻿namespace MiniORM
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MiniORM
 {
-	// Holds collecton of real entities
-	// Corresponds to a table in the database
+	public class DbSet<TEntity> : ICollection<TEntity>
+        where TEntity : class, new()
+    {
+        // Read from the DB
+        // Stored in-memory (not in the DB itself)
+        internal DbSet(IEnumerable<TEntity> entities)
+        {
+            this.Entities = entities.ToList();
+            this.ChangeTracker = new ChangeTracker<TEntity>(entities);
+        }
+
+        internal ChangeTracker<TEntity> ChangeTracker { get; set; }
+
+        internal IList<TEntity> Entities { get; set; }
+
+        public int Count => this.Entities.Count;
+
+        public bool IsReadOnly => this.Entities.IsReadOnly;
+
+        public void Add(TEntity item)
+        {         
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), ExceptionMessages.ItemNullException);
+            }
+
+            this.Entities.Add(item);
+            this.ChangeTracker.Add(item);
+        }
+
+        public bool Remove(TEntity item)
+        {          
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), ExceptionMessages.ItemNullException);
+            }
+
+            bool removedSuccessfully = this.Entities.Remove(item);
+            if (removedSuccessfully)
+            {
+                this.ChangeTracker.Remove(item);
+            }
+
+            return removedSuccessfully;
+        }
+
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                this.Remove(entity);
+            }
+        }
+
+        public void Clear()
+        {
+            while (this.Entities.Any())
+            {
+                var currenEntity = this.Entities.First();
+                this.Remove(currenEntity);
+            }
+        }
+
+        public bool Contains(TEntity item)
+            => this.Entities.Contains(item);
+
+        public void CopyTo(TEntity[] array, int arrayIndex)    
+            => this.Entities.CopyTo(array, arrayIndex);
+
+        public IEnumerator<TEntity> GetEnumerator() => this.Entities.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+    }
 }
